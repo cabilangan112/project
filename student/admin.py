489e8dadaddd5ef43e6cbe593d4e6a4024edf285
cabilangan112 <jassencabilangan@gmail.com>
2017-07-15 15:29:38 +0800

    Course

--- student/admin.py
+++ student/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Detail
+from .models import Course
 
+admin.site.register(Course)
+admin.site.register(Detail)
 # Register your models here.
--- student/models.py
+++ student/models.py
@@ -3,11 +3,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Details(models.Model):
-	first_name =  models.CharField(max_length=150)
-	last_name =  models.CharField(max_length=150)
-	MI =  models.CharField(max_length=150)
-	age =  models.IntegerField()
-	birthday = models.DateTimeField()
-	Course = models.CharField(max_length=100)
+# Create your models here.
+class Detail(models.Model):
+	first_name = models.CharField(max_length=150)
+	last_name = models.CharField(max_length=150)
+	middle_name = models.CharField(max_length=150)
+	age = models.IntegerField()
+	b_day = models.DateTimeField()
+	course = models.ManytoManyField("Course",related_name="Detail")
 	
+	def __str__(self):
+		return "{} by {}" .format(self.first_name),self.list_course())
+
+	def list_course(self):
+		return "," .join([course.course_name for course in self.course.all()])
+
+	def save(self, *args,**kwargs):
+		super(Details,self).save(*args,**kwargs)
+
+class Course(models.Model):
+	course_name = models.CharField(max_length=150)
+	course_description = models.TextField(max_length=550)
+	
+	def __str__(self):
+		return self.course_name
